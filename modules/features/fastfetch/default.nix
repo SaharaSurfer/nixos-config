b186a0d4f335ab4f4c00aac30b6635d5a50935ff
SaharaSurfer <topbo3abp@gmail.com>
2026-07-23 16:47:20 +0500

    fix(vm): update logo path evaluation in fastfetch

--- modules/features/fastfetch/default.nix
+++ modules/features/fastfetch/default.nix
@@ -9,8 +9,18 @@
     packages.fastfetch = inputs.wrapper-modules.wrappers.fastfetch.wrap {
       inherit pkgs;
       settings = 
-        (builtins.fromJSON
-          (builtins.readFile ./config.jsonc));
+        (builtins.fromJSON (builtins.readFile ./config.jsonc))
+        // {
+          logo = {
+            type = "file";
+            source = "${./logo.txt}";
+            padding = {
+              top = 1;
+              left = 1;
+              right = 2;
+            };
+          };
+        };
     };
   };
 }
