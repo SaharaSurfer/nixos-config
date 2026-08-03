5aea9b06f4963c8a5382e09fa30c75ffb548dd10
SaharaSurfer <topbo3abp@gmail.com>
2026-08-03 23:49:04 +0500

    docs(vm): add note on obsidian configuration

--- modules/features/obsidian.nix
+++ modules/features/obsidian.nix
@@ -1,5 +1,9 @@
 {
   flake.nixosModules.obsidian = { pkgs, ... }: {
+    /*
+    Obsidian configuration is stored along with the
+    vault in .obsidian directory
+    */
     environment.systemPackages = [
       pkgs.obsidian
     ];
