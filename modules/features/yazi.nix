2fc4bd08115617ffb680bd7ed0589a1dbf353d92
SaharaSurfer <topbo3abp@gmail.com>
2026-08-05 21:54:22 +0500

    refactor(vm): move yazi dependencies into runtimePkgs

--- modules/features/yazi.nix
+++ modules/features/yazi.nix
@@ -3,28 +3,32 @@
   flake.nixosModules.yazi = { pkgs, ... }: {
     environment.systemPackages = [
       self.packages.${pkgs.stdenv.hostPlatform.system}.yazi
-
-      # previews
-      pkgs.ffmpeg       # video
-      pkgs.jq           # json
-      pkgs.poppler      # pdf
-      pkgs.resvg        # svg
-      pkgs.imagemagick  # fonts, heic, etc.
-
-      # navigation
-      pkgs.fd           # file searching
-      pkgs.fzf          # subtree navigation
-      pkgs.zoxide       # better cd
-      pkgs.ripgrep      # file content searching
     ];
   };
 
-  perSystem = { pkgs, ...}: {
+  perSystem = { system, pkgs, ...}: {
     packages.yazi = inputs.wrapper-modules.wrappers.yazi.wrap {
       inherit pkgs;
       package = pkgs.yazi.override {
         _7zz = pkgs._7zz-rar;
       };
+
+      runtimePkgs = [
+        self.packages.${system}.imv
+
+        # previews
+        pkgs.ffmpeg       # video
+        pkgs.jq           # json
+        pkgs.poppler      # pdf
+        pkgs.resvg        # svg
+        pkgs.imagemagick  # fonts, heic, etc.
+
+        # navigation
+        pkgs.fd           # file searching
+        pkgs.fzf          # subtree navigation
+        pkgs.zoxide       # better cd
+        pkgs.ripgrep      # file content searching
+      ];
     };
   };
 }
